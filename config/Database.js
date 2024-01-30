dd1192cc2d82efd1f203bbaa0c7e954470994503
f0ursecond <2ez4second@gmail.com>
2024-01-30 14:25:40 +0700

    feat: prepare deploy(vercel) 3

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
 const db = new Sequelize("bztr2yjwsf0kmrba7sqo", "u4ufwid7qzszwz7z", "n31GOHQ2j5FobAZyLvkU", {
   host: "bztr2yjwsf0kmrba7sqo-mysql.services.clever-cloud.com",
   dialect: "mysql",
+  dialectModule: mysql2,
 });
 
 export default db;
