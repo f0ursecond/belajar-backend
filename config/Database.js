7eca14aba50af3682d3c207b82a9f2f9b719d13d
f0ursecond <2ez4second@gmail.com>
2024-01-30 14:06:39 +0700

    feat: prepare deploy(vercel)

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("crudtest", "root", "", {
-  host: "localhost",
+const db = new Sequelize("bztr2yjwsf0kmrba7sqo", "u4ufwid7qzszwz7z", "n31GOHQ2j5FobAZyLvkU", {
+  host: "bztr2yjwsf0kmrba7sqo-mysql.services.clever-cloud.com",
   dialect: "mysql",
 });
 
