1a093e503bed70aef3e366587c9a9306c77577f8
= <=>
2024-01-31 20:54:09 +0700

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,73 @@
+Simple CRUD Backend I made using Express JS,Mysql & ChatGPT.
+
+> tipis tipis 🔥 🔥 🔥.\
+>  menyala abangkuh 🔥 🔥 🔥.
+
+## Register
+
+endpoint : https://belajar-backend-okev.vercel.app/api/register
+
+method : **`POST`**\
+form-data: **`username`**,**`password`**\
+headers: **`Accept: application/json`**
+
+### Example Response :
+
+```
+{
+    "msg": "User Successfully Created",
+    "user": {
+        "userId": "a1f5d19c-db39-4715-8300-b3c467c189c8",
+        "username": "anjaymabar",
+        "updatedAt": "2024-01-31T13:24:09.838Z",
+        "createdAt": "2024-01-31T13:24:09.838Z"
+    }
+}
+```
+
+> **Note:** We apologize for any inconvenience you may be experiencing. Currently, our servers are located at a considerable distance, impacting performance.
+
+## Login
+
+endpoint : https://belajar-backend-okev.vercel.app/api/login
+
+method : **`POST`**\
+form-data: **`username`**,**`password`**\
+headers: **`Accept: application/json`**
+
+### Example Response :
+
+```
+{
+"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1cGVybWFuIiwicGFzc3dvcmQiOiIxMjM0NSIsImlhdCI6MTcwNjcwNTc4NSwiZXhwIjoxNzA2NzA5Mzg1fQ.nVL4FNPTMNh65YhwyeVsKp8cpJ6pZqyFyNihcO0MW7Y"
+}
+```
+
+## Get All Products
+
+endpoint : https://belajar-backend-okev.vercel.app/api/products
+
+method : **`GET`**\
+authorization: **`Bearer Token`**\
+headers: **`Accept: application/json`**
+
+### Example Response :
+
+```
+[
+    {
+        "id": 3,
+        "name": "Laptop",
+        "imageUrl": [
+            "https://ucarecdn.com/8a0e775b-df28-45d1-af5d-bdeaa2b77f85/"
+        ]
+    },
+    {
+        "id": 4,
+        "name": "Camera",
+        "imageUrl": [
+            "https://ucarecdn.com/f4c105f7-6e5a-4518-a4f1-461391f24449/"
+        ]
+    }
+]
+```
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,60 +1,63 @@
 import User from "../models/UserModel.js";
-import { v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const saltRounds = 10;
 let token = "";
-const secretKey = 'my_secret_key';
+const secretKey = "my_secret_key";
 
 //ANJAYYY
 
 export const register = async (req, res) => {
-    try {
-        const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-        const response = await User.findAll();
-        const usernameExist = response.some(user => user.username === username);
+    const response = await User.findAll();
+    const usernameExist = response.some((user) => user.username === username);
 
-        if (usernameExist) return res.status(400).json({ msg: 'Username Already Used' })
+    if (usernameExist)
+      return res.status(400).json({ msg: "Username Already Used" });
 
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-        const newUser = await User.create({
-            userId: uuidv4(),
-            username: username,
-            password: hashedPassword,
-        })
-        return res.status(201).json({ msg: 'User Successfully Created', user: newUser });
-    } catch (error) {
-        return res.status(400).json({ msg: `Error ${error}` })
-    }
-}
+    const newUser = await User.create({
+      userId: uuidv4(),
+      username: username,
+    });
+    return res
+      .status(201)
+      .json({ msg: "User Successfully Created", user: newUser });
+  } catch (error) {
+    return res.status(400).json({ msg: `Error ${error}` });
+  }
+};
 
 export const login = async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ where: { username: username } });
+  const { username, password } = req.body;
+  const user = await User.findOne({ where: { username: username } });
 
-    if (user) {
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (user) {
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        if (isPasswordValid) {
-            token = jwt.sign({ "username": username, "password": password }, secretKey, { expiresIn: '1h' });
-            res.cookie('accessToken', token, { httpOnly: true, secure: true });
-            res.status(200).json({ token: token });
-        } else {
-            res.status(400).json({ msg: 'Wrong Password' })
-        }
-
+    if (isPasswordValid) {
+      token = jwt.sign({ username: username, password: password }, secretKey, {
+        expiresIn: "1h",
+      });
+      res.cookie("accessToken", token, { httpOnly: true, secure: true });
+      res.status(200).json({ token: token });
     } else {
-        res.status(404).json({ msg: 'Username Not Found' })
+      res.status(400).json({ msg: "Wrong Password" });
     }
-}
+  } else {
+    res.status(404).json({ msg: "Username Not Found" });
+  }
+};
 
 export const logout = async (req, res) => {
-    try {
-        //CLEAR TOKEN IN FRONT END
-        res.status(200).json({ message: 'Logout successful' });
-    } catch (error) {
-        return res.status(500).json({ msg: 'Internal Server Error' + error })
-    }
-}
+  try {
+    //CLEAR TOKEN IN FRONT END
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    return res.status(500).json({ msg: "Internal Server Error" + error });
+  }
+};
